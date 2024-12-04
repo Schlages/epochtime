da4e2ec970daef83a8650bcd7d312d4f045e44ca
Schlages <bschlagel91@gmail.com>
2024-12-04 12:13:54 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+epoch_time = time.time()
+
+@app.route('/epochtime/')
+def get_time():
+    epoch_time = time.time()
+
+    return f"Current epoch time: {epoch_time}"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
